2a3283d2ec192a6e21a42ece0c6bcbdb33fec98a
tjmakunike <tjmakunike@gmail.com>
2015-12-17 15:07:23 +0200

    First Commit

---
+++ cvblog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+	thumbnail = models.CharField(max_length=250)
+	article_image = models.CharField(max_length=200)
+	
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+		
+	def __str__(self):
+		return self.title
+	
