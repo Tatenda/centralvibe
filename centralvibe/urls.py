ec2a673ee3bca6260419bdbde0f0845271e17c54
tjmakunike <tjmakunike@gmail.com>
2015-12-18 14:04:14 +0200

    First HTML commit

--- centralvibe/urls.py
+++ centralvibe/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from cvblog import urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+	url(r'', include(urls)),
 ]
